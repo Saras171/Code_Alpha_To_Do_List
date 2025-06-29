d102800cf45245027994b3cb68b3b8b060b6a137
Saraswati Rawat <officialsaraswatirawat@gmail.com>
2025-06-29 12:27:25 +0530

    Update token.js

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -8,16 +8,16 @@ import jwt from "jsonwebtoken";
  *              in the browser as an HTTP-only cookie to ensure security.
  *
  * @param {string} userId - The unique identifier of the authenticated user.
- * @returns {string} - A signed JWT token valid for 1 day.
+ * @returns {string} - A signed JWT token valid for 10 minutes.
  *
  * Note:
  * - The token payload contains the userId.
  * - The secret used for signing is pulled from environment variables (JWT_SECRET).
- * - Expiration time is set to 1 day for secure session handling.
+ * - Expiration time is set to 10 minutes for secure session handling.
  */
 export const generateToken = (userId) => {
   return jwt.sign({ userId }, // Payload: user identity
      process.env.JWT_SECRET,   // Secret key for signing (should be strong and private)
-     { expiresIn: "10m", }      // Token expires in 1 day
+     { expiresIn: "10m", }      // Token expires in 10 minutes
     );
 };
